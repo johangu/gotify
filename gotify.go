ac18504de5eb36d698ddcdcf36949352d564fa3f
Johan Guttormsson <johan@jgsoft.se>
2014-06-20 22:05:27 +0200

    added some more api calls

--- gotify.go
+++ gotify.go
@@ -24,19 +24,28 @@ func (e SpotifyError) Error() string {
 	return fmt.Sprintf("%d, code: %d - %s", e.HttpStatus, e.Code, e.Message)
 }
 
-func call(call string, verb string, parameters url.Values, token Token) (string, error) {
-	Url := fmt.Sprintf("%s%s", APIUrl, call)
+func call(call string, verb string, params url.Values, token Token) (string, error) {
+	verb = strings.ToUpper(verb)
+	var body string
+	var Url string
+
+	if verb == "GET" {
+		Url = fmt.Sprintf("%s%s?%s", APIUrl, call, params.Encode())
+	} else {
+		Url = fmt.Sprintf("%s%s", APIUrl, call)
+	}
 	client := &http.Client{}
 	resp := &http.Response{}
-	var body string
-	req, err := http.NewRequest(strings.ToUpper(verb), Url, strings.NewReader(parameters.Encode()))
+
+	req, err := http.NewRequest(verb, Url, strings.NewReader(params.Encode()))
 	if err == nil {
-		if strings.ToUpper(verb) == "POST" {
+		if verb == "POST" {
 			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 		}
 		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
 		resp, err = client.Do(req)
 		defer resp.Body.Close()
+		fmt.Println(resp.Request)
 		if err == nil {
 			if resp.StatusCode == http.StatusOK {
 				tmp, err := ioutil.ReadAll(resp.Body)
@@ -64,7 +73,15 @@ func Album(id string, token Token) (string, error) {
 // Returns a list of albums
 func Albums(albums []string, token Token) (string, error) {
 	ids := buildIdString(albums, "album")
-	return call(fmt.Sprintf("albums?ids=%s", ids), "GET", url.Values{}, token)
+	params := url.Values{}
+	params.Add("ids", ids)
+	return call("albums", "GET", params, token)
+}
+
+// Returns tracks information from an album given an album id
+func AlbumTracks(id string, token Token) (string, error) {
+	id = parseId("album", id)
+	return call(fmt.Sprintf("albums/%s/tracks", id), "GET", url.Values{}, token)
 }
 
 // Returns an artist
@@ -73,10 +90,26 @@ func Artist(id string, token Token) (string, error) {
 	return call(fmt.Sprintf("artists/%s", id), "GET", url.Values{}, token)
 }
 
+// Returns albums from an artist, given an artist id
+func ArtistAlbums(id string, token Token) (string, error) {
+	id = parseId("artist", id)
+	return call(fmt.Sprintf("artists/%s/albums", id), "GET", url.Values{}, token)
+}
+
+// Returns an artists top tracks given artist id and country
+func ArtistTopTracks(id string, country string, token Token) (string, error) {
+	id = parseId("artist", id)
+	params := url.Values{}
+	params.Add("country", country)
+	return call(fmt.Sprintf("artists/%s/top-tracks", id), "GET", params, token)
+}
+
 // Returns a list of artists
 func Artists(artists []string, token Token) (string, error) {
 	ids := buildIdString(artists, "artist")
-	return call(fmt.Sprintf("artists?ids=%s", ids), "GET", url.Values{}, token)
+	params := url.Values{}
+	params.Add("ids", ids)
+	return call("artists", "GET", params, token)
 }
 
 // Returns information about me
@@ -87,7 +120,7 @@ func Me(token Token) (string, error) {
 // Returns a playlist
 func Playlist(userId string, playlistId string, fields string, token Token) (string, error) {
 	params := url.Values{}
-	params.Add("fields", fields)
+	params.Add("fields", url.QueryEscape(fields))
 	return call(fmt.Sprintf("users/%s/playlists/%s", userId, playlistId), "GET", params, token)
 }
 
@@ -96,15 +129,34 @@ func Playlists(userId string, token Token) (string, error) {
 	return call(fmt.Sprintf("users/%s/playlists", userId), "GET", url.Values{}, token)
 }
 
+// Returns a tracks from playlists
+func Playlists(userId string, playlistId string, fields string, token Token) (string, error) {
+	params := url.Values{}
+	params.Add("fields", url.QueryEscape(fields))
+	return call(fmt.Sprintf("users/%s/playlists/%s/tracks", userId, playlistId), "GET", params, token)
+}
+
+func Search(q string, qType string, limit int, offset int, token Token) (string, error) {
+	params := url.Values{}
+	params.Add("q", q)
+	params.Add("type", url.QueryEscape(qType))
+	params.Add("limit", fmt.Sprintf("%d", limit))
+	params.Add("offset", fmt.Sprintf("%d", offset))
+
+	return call("search", "GET", params, token)
+}
+
 // Returns a single track
-func Track(trackId string, token Token) (string, error) {
-	return call(fmt.Sprintf("tracks/%s", trackId), "GET", url.Values{}, token)
+func Track(id string, token Token) (string, error) {
+	return call(fmt.Sprintf("tracks/%s", id), "GET", url.Values{}, token)
 }
 
 // Returns a list of tracks
 func Tracks(tracks []string, token Token) (string, error) {
 	ids := buildIdString(tracks, "track")
-	return call(fmt.Sprintf("tracks?ids=%s", ids), "GET", url.Values{}, token)
+	params := url.Values{}
+	params.Add("ids", ids)
+	return call("tracks", "GET", params, token)
 }
 
 // Gets basic information about a user
