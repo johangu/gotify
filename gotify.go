483a208206f3321b3b92b6faf9c8918d715951e6
Johan Guttormsson <johan@jgsoft.se>
2014-06-21 02:53:56 +0200

    implemened all calls, should work, needs tidying up

--- gotify.go
+++ gotify.go
@@ -2,6 +2,8 @@
 package gotify
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,59 +16,57 @@ const Version = 1
 
 var APIUrl = fmt.Sprintf("https://api.spotify.com/v%d/", Version)
 
-type SpotifyError struct {
-	HttpStatus int
-	Code       int
-	Message    string
-}
-
-func (e SpotifyError) Error() string {
-	return fmt.Sprintf("%d, code: %d - %s", e.HttpStatus, e.Code, e.Message)
-}
-
 func call(call string, verb string, params url.Values, token Token) (string, error) {
 	verb = strings.ToUpper(verb)
-	var body string
+	var respBody string
 	var Url string
-
-	if verb == "GET" {
-		Url = fmt.Sprintf("%s%s?%s", APIUrl, call, params.Encode())
-	} else {
+	if params == nil {
 		Url = fmt.Sprintf("%s%s", APIUrl, call)
+	} else {
+		Url = fmt.Sprintf("%s%s?%s", APIUrl, call, params.Encode())
 	}
 	client := &http.Client{}
 	resp := &http.Response{}
 
-	req, err := http.NewRequest(verb, Url, strings.NewReader(params.Encode()))
+	body, err := json.Marshal(params)
+	strippedBody := strings.Replace(strings.Replace(string(body), "[", "", -1), "]", "", -1)
+
+	req, err := http.NewRequest(verb, Url, strings.NewReader(string(strippedBody)))
 	if err == nil {
 		if verb == "POST" {
-			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			req.Header.Set("Content-Type", "application/json")
 		}
 		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
 		resp, err = client.Do(req)
 		defer resp.Body.Close()
 		if err == nil {
-			if resp.StatusCode == http.StatusOK {
+			if resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusCreated {
+				// Better way to get the respone body to string? feel free to patch
 				tmp, err := ioutil.ReadAll(resp.Body)
 				if err == nil {
-					body = string(tmp)
-					return body, nil
+					respBody = string(tmp)
+					return respBody, nil
 				}
 			} else {
-				err = SpotifyError{
-					resp.StatusCode,
-					-1,
-					"Requested resource could not be found"}
+				err = errors.New(resp.Status)
 			}
 		}
 	}
 	return "", err
 }
 
+func AddTracksToPlaylist(userId string, playlistId string, uris []string, token Token) (string, error) {
+	//	params := map[string][]string{
+	//		"uris": uris,
+	//	}
+	ids := strings.Join(uris, ",")
+	return call(fmt.Sprintf("users/%s/playlists/%s/tracks?uris=%s", userId, playlistId, ids), "POST", nil, token)
+}
+
 // Returns an album
 func Album(id string, token Token) (string, error) {
 	id = parseId("album", id)
-	return call(fmt.Sprintf("albums/%s", id), "GET", url.Values{}, token)
+	return call(fmt.Sprintf("albums/%s", id), "GET", nil, token)
 }
 
 // Returns a list of albums
@@ -80,19 +80,19 @@ func Albums(albums []string, token Token) (string, error) {
 // Returns tracks information from an album given an album id
 func AlbumTracks(id string, token Token) (string, error) {
 	id = parseId("album", id)
-	return call(fmt.Sprintf("albums/%s/tracks", id), "GET", url.Values{}, token)
+	return call(fmt.Sprintf("albums/%s/tracks", id), "GET", nil, token)
 }
 
 // Returns an artist
 func Artist(id string, token Token) (string, error) {
 	id = parseId("artist", id)
-	return call(fmt.Sprintf("artists/%s", id), "GET", url.Values{}, token)
+	return call(fmt.Sprintf("artists/%s", id), "GET", nil, token)
 }
 
 // Returns albums from an artist, given an artist id
 func ArtistAlbums(id string, token Token) (string, error) {
 	id = parseId("artist", id)
-	return call(fmt.Sprintf("artists/%s/albums", id), "GET", url.Values{}, token)
+	return call(fmt.Sprintf("artists/%s/albums", id), "GET", nil, token)
 }
 
 // Returns an artists top tracks given artist id and country
@@ -111,9 +111,17 @@ func Artists(artists []string, token Token) (string, error) {
 	return call("artists", "GET", params, token)
 }
 
+// Creates a playlist for the given user
+func CreatePlaylist(userId string, name string, public bool, token Token) (string, error) {
+	params := url.Values{}
+	params.Add("name", name)
+	params.Add("public", fmt.Sprintf("%v", public))
+	return call(fmt.Sprintf("users/%s/playlists", userId), "POST", params, token)
+}
+
 // Returns information about me
 func Me(token Token) (string, error) {
-	return call("me", "GET", url.Values{}, token)
+	return call("me", "GET", nil, token)
 }
 
 // Returns a playlist
@@ -125,11 +133,11 @@ func Playlist(userId string, playlistId string, fields string, token Token) (str
 
 // Returns a users playlists
 func Playlists(userId string, token Token) (string, error) {
-	return call(fmt.Sprintf("users/%s/playlists", userId), "GET", url.Values{}, token)
+	return call(fmt.Sprintf("users/%s/playlists", userId), "GET", nil, token)
 }
 
 // Returns a tracks from playlists
-func Playlists(userId string, playlistId string, fields string, token Token) (string, error) {
+func PlaylistTracks(userId string, playlistId string, fields string, token Token) (string, error) {
 	params := url.Values{}
 	params.Add("fields", url.QueryEscape(fields))
 	return call(fmt.Sprintf("users/%s/playlists/%s/tracks", userId, playlistId), "GET", params, token)
@@ -147,7 +155,7 @@ func Search(q string, qType string, limit int, offset int, token Token) (string,
 
 // Returns a single track
 func Track(id string, token Token) (string, error) {
-	return call(fmt.Sprintf("tracks/%s", id), "GET", url.Values{}, token)
+	return call(fmt.Sprintf("tracks/%s", id), "GET", nil, token)
 }
 
 // Returns a list of tracks
@@ -160,7 +168,7 @@ func Tracks(tracks []string, token Token) (string, error) {
 
 // Gets basic information about a user
 func User(userId string, token Token) (string, error) {
-	return call(fmt.Sprintf("users/%s", userId), "GET", url.Values{}, token)
+	return call(fmt.Sprintf("users/%s", userId), "GET", nil, token)
 }
 
 func buildIdString(slice []string, idType string) string {
@@ -181,7 +189,6 @@ func parseId(idType string, uri string) string {
 		return fields[2]
 	}
 	fields = strings.Split(uri, "/")
-	fmt.Println(len(fields))
 	if len(fields) >= 3 {
 		if idType != fields[len(fields)-2] {
 			log.Print(fmt.Sprintf("Warning: expected if of type %s but found id of type %s", idType, fields[1]))
